aa3eba721eed50a31928ade5b87dd2ae3c65622c
Nick Van den Broeck <vandenbroeck@cs-vdb.com>
2021-07-11 09:44:58 +0200

    Implemented concept of Exercises and inserted into dB

---
+++ pact-api/src/Pact/API/Data/Exercise.hs
@@ -0,0 +1,121 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Pact.API.Data.Exercise where
+
+import Data.Aeson
+import Data.Either.Combinators (mapLeft)
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Validity
+import Data.Validity.Text ()
+import Database.Persist
+import Database.Persist.Sql
+import GHC.Generics (Generic)
+import Servant.Client.Core (BaseUrl, parseBaseUrl, showBaseUrl)
+import Text.Read (readEither)
+
+data Difficulty = Easy | Medium | Hard deriving (Show, Eq, Ord, Generic, Read)
+
+instance Validity Difficulty -- Any value is valid
+
+instance FromJSON Difficulty
+
+instance ToJSON Difficulty
+
+instance PersistField Difficulty where
+  toPersistValue = toPersistValue . show
+  fromPersistValue v = mapLeft T.pack . readEither =<< fromPersistValue v
+
+instance PersistFieldSql Difficulty where
+  sqlType _ = SqlString
+
+data WorkoutType
+  = Push
+  | Pull
+  | Core
+  | Legs
+  | FullBody
+  | Cardio
+  deriving (Show, Eq, Ord, Generic)
+
+data MuscleGroup
+  = Shoulders
+  | Abs
+  | Biceps
+  | Triceps
+  | Forearms
+  | Quads
+  | Calves
+  | Hamstrings
+  | UpperChest
+  | LowerChest
+  | UpperBack
+  | LowerBack
+  deriving (Show, Eq, Ord, Generic, Read)
+
+instance Validity MuscleGroup -- Any value is valid
+
+instance FromJSON MuscleGroup
+
+instance ToJSON MuscleGroup
+
+instance PersistField MuscleGroup where
+  toPersistValue = toPersistValue . show
+  fromPersistValue v = mapLeft T.pack . readEither =<< fromPersistValue v
+
+instance PersistFieldSql MuscleGroup where
+  sqlType _ = SqlString
+
+-- TODO: Serve all images and videos on our own website (e.g.
+-- `static.pactcommunity.be`), generate a hash for each, and replace `BaseUrl`
+-- by `hash + ".png"` or "hash + ".mp4"`.
+type SourceURI = BaseUrl
+
+instance Validity BaseUrl where
+  validate baseUrl = case parseBaseUrl $ showBaseUrl baseUrl of
+    Left err -> invalid $ show err
+    Right _ -> valid
+
+instance PersistField BaseUrl where
+  toPersistValue = toPersistValue . showBaseUrl
+  fromPersistValue (PersistText t) = case parseBaseUrl $ T.unpack t of
+    Left err -> Left $ T.pack $ show err
+    Right baseUrl -> Right baseUrl
+  fromPersistValue _ = Left "No text"
+
+instance PersistFieldSql BaseUrl where
+  sqlType _ = SqlString
+
+newtype FormTip = FormTip {unFormTip :: Text} deriving (Show, Eq, Ord, Generic)
+
+mkFormTip :: Text -> Maybe FormTip
+mkFormTip t
+  | T.any (== '\n') t = Nothing
+  | otherwise = Just $ FormTip t
+
+instance Validity FormTip where
+  validate (FormTip t) =
+    mconcat
+      [ check (not $ T.any (== '\n') t) "FormTip doesn't contain newlines",
+        delve "FormTip contains a valid Text" t
+      ]
+
+instance FromJSON FormTip where
+  parseJSON = withText "FormTip" $ \t ->
+    if T.any (== '\n') t
+      then fail "FormTips cannot contain newlines"
+      else pure $ FormTip t
+
+instance ToJSON FormTip
+
+instance PersistField FormTip where
+  toPersistValue (FormTip t) = toPersistValue t
+  fromPersistValue (PersistText t) = case mkFormTip t of
+    Nothing -> Left "Contains newlines"
+    Just formTip -> Right formTip
+  fromPersistValue _ = Left "Not text"
+
+instance PersistFieldSql FormTip where
+  sqlType _ = SqlString
---
+++ pact-api/src/Pact/API/Data/User.hs
@@ -0,0 +1,156 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+module Pact.API.Data.User where
+
+import Data.Aeson
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Validity
+import Data.Validity.Text ()
+import Database.Persist
+import Database.Persist.Sql
+import Servant.API.Generic
+import Servant.Auth.Server
+import YamlParse.Applicative
+
+newtype EmailAddress = EmailAddress String deriving (Show, Eq, Ord, Generic)
+
+instance Validity EmailAddress
+
+instance ToJSON EmailAddress where
+  toJSON (EmailAddress s) = String $ T.pack s
+
+instance FromJSON EmailAddress where
+  parseJSON = withText "EmailAddress" $ pure . EmailAddress . T.unpack
+
+instance PersistField EmailAddress where
+  toPersistValue (EmailAddress s) = toPersistValue s
+  fromPersistValue v = EmailAddress <$> fromPersistValue v
+
+instance PersistFieldSql EmailAddress where
+  sqlType _ = SqlString
+
+data RegistrationForm = RegistrationForm
+  { registrationFormUsername :: Username,
+    registrationFormPassword :: Text,
+    registrationFormEmail :: EmailAddress
+  }
+  deriving (Show, Eq, Ord, Generic)
+
+instance Validity RegistrationForm where
+  validate rf@RegistrationForm {..} =
+    mconcat
+      [ genericValidate rf,
+        declare "The password is nonempty" $
+          not $
+            T.null registrationFormPassword
+      ]
+
+instance ToJSON RegistrationForm where
+  toJSON RegistrationForm {..} =
+    object
+      [ "username" .= registrationFormUsername,
+        "password" .= registrationFormPassword,
+        "email" .= registrationFormEmail
+      ]
+
+instance FromJSON RegistrationForm where
+  parseJSON = withObject "RegistrationForm" $ \o ->
+    RegistrationForm <$> o .: "username" <*> o .: "password" <*> o .: "email"
+
+data LoginForm = LoginForm
+  { loginFormUsername :: Username,
+    loginFormPassword :: Text
+  }
+  deriving (Show, Eq, Ord, Generic)
+
+instance Validity LoginForm
+
+instance ToJSON LoginForm where
+  toJSON LoginForm {..} =
+    object
+      ["username" .= loginFormUsername, "password" .= loginFormPassword]
+
+instance FromJSON LoginForm where
+  parseJSON = withObject "LoginForm" $ \o ->
+    LoginForm <$> o .: "username" <*> o .: "password"
+
+data AuthCookie = AuthCookie
+  { authCookieUsername :: Username
+  }
+  deriving (Show, Eq, Ord, Generic)
+
+instance FromJSON AuthCookie
+
+instance ToJSON AuthCookie
+
+instance FromJWT AuthCookie
+
+instance ToJWT AuthCookie
+
+newtype Username = Username
+  { usernameText :: Text
+  }
+  deriving (Show, Eq, Ord, Generic, FromJSONKey, ToJSONKey)
+
+instance Validity Username where
+  validate (Username t) =
+    mconcat
+      [ check (not (T.null t)) "The username is not empty.",
+        check
+          (T.length t >= 3)
+          "The username is at least three characters long."
+      ]
+
+instance PersistField Username where
+  toPersistValue (Username t) = PersistText t
+  fromPersistValue (PersistText t) =
+    case parseUsername t of
+      Nothing -> Left "Text isn't a valid username"
+      Just un -> Right un
+  fromPersistValue _ = Left "Not text"
+
+instance PersistFieldSql Username where
+  sqlType _ = SqlString
+
+instance YamlSchema Username where
+  yamlSchema = eitherParser parseUsernameOrErr yamlSchema
+
+instance FromJSON Username where
+  parseJSON = withText "Username" $ pure . Username
+
+instance ToJSON Username where
+  toJSON (Username t) = String t
+
+parseUsername :: Text -> Maybe Username
+parseUsername = constructValid . Username
+
+parseUsernameOrErr :: Text -> Either String Username
+parseUsernameOrErr = prettyValidate . Username
+
+data Profile = Profile
+  { profileName :: Username,
+    profileEmail :: EmailAddress
+  }
+  deriving (Show, Eq, Ord, Generic)
+
+instance Validity Profile
+
+instance ToJSON Profile where
+  toJSON Profile {..} =
+    object
+      [ "username" .= profileName,
+        "email" .= profileEmail
+      ]
+
+instance FromJSON Profile where
+  parseJSON = withObject "Profile" $ \o ->
+    Profile <$> o .: "username" <*> o .: "email"

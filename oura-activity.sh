6fd2c7b318d50c2c3ec5a604bcf4908506cef30c
Nick Van den Broeck <vandenbroeck@cs-vdb.com>
2023-11-07 16:49:14 +0100

    update

--- oura-activity.sh
+++ oura-activity.sh
@@ -1,1 +1,4 @@
-curl --location --request GET 'https://api.ouraring.com/v2/usercollection/daily_activity?start_date=2023-11-01&end_date=2023-11-07' --header "Authorization: Bearer $OURA_KEY"
+curl --request GET 'https://api.ouraring.com/v2/usercollection/daily_activity?start_date=2023-11-01&end_date=2023-11-07' --header 'Authorization: Bearer 2G32LBAM3Q2ZN6TRERAK4MTWMON6AOGQ' | python -m json.tool > activity.json
+# Once you have this JSON blob, you read out `data`. This provides a list of
+# elements. In each element, you read out `date` and `score`, and put that into
+# the PACT dB.
